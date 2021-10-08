8d252e2bb57b1a180c9c2b6d0fc5b6c803f54174
Josh <josh@gdovindesigns.com>
2021-10-08 10:26:51 -0400

    FC demo files

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,8 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+
+
+let mainTL = gsap.timeline();
+
+// mainTL.to("#hero",{duration:0.5, autoAlpha:0});
+
+mainTL.from("h1",{duration:1, y:"+=200", stagger:0.5});
