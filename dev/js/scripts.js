bd4828ef30f49650d69af0ee248246e8c118ab0e
Josh <josh@gdovindesigns.com>
2021-10-20 13:41:49 -0400

    working demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,6 +2,70 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
+function ballOne(){
+    const tl =gsap.timeline();
+    tl.to("#ball-1",{duration:1, scale:3});
+    return tl;
+}
+
+
+function ballTwo(){
+    const tl =gsap.timeline();
+    tl.to("#ball-2",{duration:1, scale:3});
+    return tl;
+}
+
+mainTL.add(ballTwo())
+        .add(ballOne());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // reference the timeline | Do we want to start at the beginning or the end of the timeline? | What thing to you want to animate? | duration of the animation? | What do we want to do?
 
 // from = start point of timeline
@@ -10,14 +74,14 @@ const mainTL = gsap.timeline();
 // to = end point of timeline
 // mainTL.to("#hero article",{duration:2,alpha:0, scale:2});
 
-const heroHeight = document.querySelector("#hero");
+// const heroHeight = document.querySelector("#hero");
 
-const aniTime = 0.5;
+// const aniTime = 0.5;
 
-console.log(heroHeight.clientHeight + "px is the height of the hero section");
-console.log(heroHeight.clientWidth + "px is the width of the hero section");
+// console.log(heroHeight.clientHeight + "px is the height of the hero section");
+// console.log(heroHeight.clientWidth + "px is the width of the hero section");
 
 
-mainTL.from("#hero article",{duration:aniTime,alpha:0, y: heroHeight.clientHeight})
-    .from("#hero h1",{duration:aniTime, y:"+=300", alpha:0, rotation:180},"madeUp")
-    .from("#hero h2",{duration:aniTime, y:"+=300", alpha:0, rotation:180}, "madeUp");
+// mainTL.from("#hero article",{duration:aniTime,alpha:0, y: heroHeight.clientHeight})
+//     .from("#hero h1",{duration:aniTime, y:"+=300", alpha:0, rotation:180},"madeUp")
+//     .from("#hero h2",{duration:aniTime, y:"+=300", alpha:0, rotation:180}, "madeUp");
