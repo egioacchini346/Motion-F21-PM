3a78cc50168123b275bcfc0c2ecd52315546c883
Josh <josh@gdovindesigns.com>
2021-10-25 13:55:12 -0400

    monday demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,32 +1,155 @@
-import { gsap } from "gsap";
-import { GSDevTools } from "gsap/GSDevTools";
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import {
+    gsap
+} from "gsap";
+import {
+    GSDevTools
+} from "gsap/GSDevTools";
+import {
+    DrawSVGPlugin
+} from "gsap/DrawSVGPlugin";
+import {
+    TextPlugin
+} from "gsap/TextPlugin";
+import {
+    MotionPathPlugin
+} from "gsap/MotionPathPlugin";
 
-gsap.registerPlugin(GSDevTools, DrawSVGPlugin);
+gsap.registerPlugin(GSDevTools, DrawSVGPlugin, TextPlugin, MotionPathPlugin);
+
+gsap.set("#line-2", {
+    scaleX: -1,
+    transformOrigin: "center center"
+});
 
 const mainTL = gsap.timeline();
-
-function ballOne(){
-    // const tl =gsap.timeline();
-
-    // tl.to("#outline",{duration:1, drawSVG:"0%"});
-    // tl.to("#ball-1",{duration:1, scale:9});
-    // return tl;
-}
+const aniTime = 0.5;
 
 
-function ballTwo(){
-    const tl =gsap.timeline();
-    tl.to("#ball-2",{duration:1, scale:3});
+function dropIn() {
+    const tl = gsap.timeline();
+    // tl.from("#line-1",{duration: 2, drawSVG:0},"same");
+    // tl.from("#line-2",{duration: 2, drawSVG:0},"same");
+
+    // start here
+    tl.from(".ball", {
+            duration: aniTime,
+            y: "-=200",
+            alpha: 0,
+            ease: "bounce.out",
+            stagger: 0.25
+        }, "start")
+        .from("#outline", {
+            duration: 0.25,
+            drawSVG: "0%"
+        }, "start")
+        .to("#welcome-text", {
+            duration: 0.25,
+            text: "LOADING",
+            ease: "none"
+        });
+
+    // -------------------
+    // see Start here for the line that replaces the 3 lines below
+    // tl.from("#ball-1",{duration: 0.5, y:"-=200", alpha:0, ease: "bounce.out"});
+    // tl.from("#ball-2",{duration: 0.5, y:"-=200", alpha:0, ease: "bounce.out"},"-=50%");
+    // tl.from("#ball-3",{duration: 0.5, y:"-=200", alpha:0, ease: "bounce.out"},"-=50%");
+    // -------------------
     return tl;
 }
 
-mainTL.add(ballTwo());
-        // .add(ballOne());
+function ballHop() {
+    const tl = gsap.timeline({
+        repeat: 2
+    });
+    tl.to("#ball-1", {
+            duration: aniTime,
+            motionPath: {
+                path: "#path-1",
+                align: "self"
+            },
+            scale: 0.5,
+            ease: "power4.out"
+        })
+        .to("#ball-2", {
+            duration: aniTime,
+            motionPath: {
+                path: "#path-2",
+                align: "self"
+            },
+            scale: 0.5,
+            ease: "power4.out"
+        }, "-=75%")
+        .to("#ball-3", {
+            duration: aniTime,
+            motionPath: {
+                path: "#path-3",
+                align: "self"
+            },
+            scale: 0.5,
+            ease: "power4.out"
+        }, "-=75%")
+        .to("#ball-1", {
+            duration: aniTime,
+            x: "-=158",
+            scale: 1
+        }, "-=50%")
+        .to("#ball-2", {
+            duration: aniTime,
+            x: "-=158",
+            scale: 1
+        }, "-=50%")
+        .to("#ball-3", {
+            duration: aniTime,
+            x: "-=158",
+            scale: 1
+        }, "-=50%")
+
+        .from("#blackOutline", {
+            duration: 1,
+            autoAlpha: 0
+        });
+
+    return tl;
+}
 
 
 
-// GSDevTools.create();
+
+
+
+mainTL.add(dropIn())
+    .add(ballHop());
+
+
+GSDevTools.create();
+
+
+
+
+
+
+
+// function ballOne(){
+//     // const tl =gsap.timeline();
+
+//     // tl.to("#outline",{duration:1, drawSVG:"0%"});
+//     // tl.to("#ball-1",{duration:1, scale:9});
+//     // return tl;
+// }
+
+
+// function ballTwo(){
+//     const tl =gsap.timeline();
+//     tl.to("#ball-2",{duration:1, scale:3});
+//     return tl;
+// }
+
+
+// .add(ballOne());
+
+
+
+
 
 
 
