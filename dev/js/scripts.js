c8bf4631950d7d99228be7d8eba44c6435e2f49d
Josh <josh@gdovindesigns.com>
2021-11-10 14:17:27 -0500

    demo complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -13,38 +13,98 @@ gsap.set("#dot",{transformOrigin: "50% 50%", xPercent:-50, yPercent:-50});
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#t-down",{duration:0.5, y:"-=500", ease:"none"})
+function tAnimation(){
+    const tl = gsap.timeline();
+    tl.from("#t-down",{duration:0.5, y:"-=500", ease:"none"})
 
- // stretch out bottom of T
- .to("#t-down",{duration:0.5, scaleY:1.5, ease: "none)"})
+    // stretch out bottom of T
+    .to("#t-down",{duration:0.5, scaleY:1.5, ease: "none)"})
 
-// animate the bottom of the T path
-.from("#t-path",{duration:0.25, scaleY:0, ease: "none)"},"-=20%")
-.fromTo("#t-path",{duration:0.25, drawSVG: "0% 10%"}, {drawSVG:"90% 100%", ease:"none"}, "tAnimation")
+    // animate the bottom of the T path
+    .from("#t-path",{duration:0.25, scaleY:0, ease: "none)"},"-=20%")
+    .fromTo("#t-path",{duration:0.25, drawSVG: "0% 10%"}, {drawSVG:"90% 100%", ease:"none"}, "tAnimation")
 
-// animate the t cross bar
-.from("#t-cross",{duration:0.5, scaleX:0, ease: "elastic.out(1, 0.3)"}, "tAnimation")
+    // animate the t cross bar
+    .from("#t-cross",{duration:0.5, scaleX:0, ease: "elastic.out(1, 0.3)"}, "tAnimation")
 
-// snap T back into shape
-.to("#t-down",{duration:0.5, scaleY:1, ease: "elastic.out(1, 0.3)"},"-=80%")
+    // snap T back into shape
+    .to("#t-down",{duration:0.5, scaleY:1, ease: "elastic.out(1, 0.3)"},"-=80%")
+    return tl;
+}
+
+function eAnimation(){
+    const tl = gsap.timeline();
+    //rotate E 
+    tl.from("#e", {duration:0.75, rotate: 120, ease: "elastic.out(1, 0.5)"},"-=0.25")
+    return tl;
+}
+
+function kAnimation(){
+    const tl = gsap.timeline();
+    tl.from("#k", {duration:0.25, y:"-=200", ease: "bounce",autoAlpha:0},"-=0.6")
+    return tl;
+}
+
+function nAnimation(){
+    const tl = gsap.timeline();
+    tl .from("#n", {duration:0.25, scaleX:0},"-=0.35")
+    return tl;
+}
+
+function iAnimation(){
+    const tl = gsap.timeline();
+    tl.from("#i",{duration:0.5, scaleY:0, ease: "elastic.out(1, 0.5)"},"-=0.6")
+    // make T path disappear
+    .to("#t-path",{duration:0.25, scaleY:0},"-=0.6")
+  
+    // dot pops out and lands
+    .from("#dot",{duration:0.001, autoAlpha:0},"-=0.55")
+    .from("#dot",{duration:1, motionPath:{path:"#dot-path", align:"#dot-path"},ease: "bounce.out"},"-=0.65");
+    return tl;
+}
+
+
+mainTL.add(eAnimation())
+.add(tAnimation())
+.add(kAnimation())
+.add(nAnimation())
+.add(iAnimation());
+
+
+
+
+// mainTL.from("#t-down",{duration:0.5, y:"-=500", ease:"none"})
+
+//  // stretch out bottom of T
+//  .to("#t-down",{duration:0.5, scaleY:1.5, ease: "none)"})
+
+// // animate the bottom of the T path
+// .from("#t-path",{duration:0.25, scaleY:0, ease: "none)"},"-=20%")
+// .fromTo("#t-path",{duration:0.25, drawSVG: "0% 10%"}, {drawSVG:"90% 100%", ease:"none"}, "tAnimation")
+
+// // animate the t cross bar
+// .from("#t-cross",{duration:0.5, scaleX:0, ease: "elastic.out(1, 0.3)"}, "tAnimation")
+
+// // snap T back into shape
+// .to("#t-down",{duration:0.5, scaleY:1, ease: "elastic.out(1, 0.3)"},"-=80%")
 
 //rotate E 
-.from("#e", {duration:0.75, rotate: 120, ease: "elastic.out(1, 0.5)"},"-=0.25")
+// .from("#e", {duration:0.75, rotate: 120, ease: "elastic.out(1, 0.5)"},"-=0.25")
 
- // drop in K
- .from("#k", {duration:0.25, y:"-=200", ease: "bounce",autoAlpha:0},"-=0.6")
+//  // drop in K
+//  .from("#k", {duration:0.25, y:"-=200", ease: "bounce",autoAlpha:0},"-=0.6")
 
- //skew the N in
- .from("#n", {duration:0.25, scaleX:0},"-=0.35")
+//  //skew the N in
+//  .from("#n", {duration:0.25, scaleX:0},"-=0.35")
 
-  // make I pop out
-  .from("#i",{duration:0.5, scaleY:0, ease: "elastic.out(1, 0.5)"},"-=0.6")
-  // make T path disappear
-  .to("#t-path",{duration:0.25, scaleY:0},"-=0.6")
+//   // make I pop out
+//   .from("#i",{duration:0.5, scaleY:0, ease: "elastic.out(1, 0.5)"},"-=0.6")
+//   // make T path disappear
+//   .to("#t-path",{duration:0.25, scaleY:0},"-=0.6")
 
-  // dot pops out and lands
-  .from("#dot",{duration:0.001, autoAlpha:0},"-=0.55")
-  .from("#dot",{duration:1, motionPath:{path:"#dot-path", align:"#dot-path"},ease: "bounce.out"},"-=0.65");
+//   // dot pops out and lands
+//   .from("#dot",{duration:0.001, autoAlpha:0},"-=0.55")
+//   .from("#dot",{duration:1, motionPath:{path:"#dot-path", align:"#dot-path"},ease: "bounce.out"},"-=0.65");
 
 
 
